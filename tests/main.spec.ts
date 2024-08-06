319d5678503ac4a614721f4f319f0d9033f86d62
evilcore29 <evilcore29@gmail.com>
2024-08-06 21:23:39 +0300

    TON Stepik Course: Lesson 4.2

--- tests/main.spec.ts
+++ tests/main.spec.ts
@@ -5,16 +5,26 @@ import { MainContract } from "../wrappers/MainContract";
 import "@ton/test-utils";
 
 describe("main.fc contract tests", () => {
-  it("should get the proper most recent sender address", async () => {
+  it("should successfully increase counter in contract and get the proper most recent sender address", async () => {
     const blockchain = await Blockchain.create();
     const codeCell = Cell.fromBoc(Buffer.from(hex, "hex"))[0];
 
-    const myContract = blockchain.openContract(await MainContract.createFromConfig({}, codeCell));
+    const initAddress = await blockchain.treasury("initAddress");
+
+    const myContract = blockchain.openContract(
+      await MainContract.createFromConfig(
+        {
+          number: 0,
+          address: initAddress.address,
+        },
+        codeCell
+      )
+    );
 
     const senderWallet = await blockchain.treasury("sender");
 
     // Отправляем внутреннее сообщение.
-    const sentMessageResult = await myContract.sendInternalMessage(senderWallet.getSender(), toNano("0.05"));
+    const sentMessageResult = await myContract.sendIncrement(senderWallet.getSender(), toNano("0.05"), 1);
 
     // Проверяем, что отправка прошла успешно.
     expect(sentMessageResult.transactions).toHaveTransaction({
@@ -26,7 +36,8 @@ describe("main.fc contract tests", () => {
     // Вызываем getter-метод контракта и проверяем, что вызов был успешным.
     const data = await myContract.getData();
 
-    // Сравниваем результаты, полученные от getter, с адресом from, который мы задали в исходном внутреннем сообщении.
+    // Сравниваем результаты, полученные от getter, с адресом from, который мы задали в исходном внутреннем сообщении.MainContract.ts
     expect(data.recent_sender.toString()).toBe(senderWallet.address.toString());
+    expect(data.number).toEqual(1);
   });
 });
--- wrappers/MainContract.ts
+++ wrappers/MainContract.ts
@@ -1,12 +1,21 @@
 import { Address, beginCell, Cell, Contract, contractAddress, ContractProvider, Sender, SendMode } from "@ton/core";
 
+export type MainContractConfig = {
+  number: number;
+  address: Address;
+};
+
+export function mainContractConfigToCell(config: MainContractConfig): Cell {
+  return beginCell().storeUint(config.number, 32).storeAddress(config.address).endCell();
+}
+
 // для того чтобы работать с восстановленным контрактом для его тестирования, рекомендуется писать обертки
 // для написания обертки по доке Sandbox нужно использовать интерфейс Contract из @ton/core
 export class MainContract implements Contract {
   constructor(readonly address: Address, readonly init?: { code: Cell; data: Cell }) {}
 
-  static createFromConfig(config: any, code: Cell, workchain = 0) {
-    const data = beginCell().endCell();
+  static createFromConfig(config: MainContractConfig, code: Cell, workchain = 0) {
+    const data = mainContractConfigToCell(config);
     const init = { code, data };
     const address = contractAddress(workchain, init);
 
@@ -14,18 +23,24 @@ export class MainContract implements Contract {
   }
 
   // отправляет внутренее сообщение
-  async sendInternalMessage(provider: ContractProvider, sender: Sender, value: bigint) {
+  async sendIncrement(provider: ContractProvider, sender: Sender, value: bigint, increment_by: number) {
+    const msg_body = beginCell()
+      .storeUint(1, 32) // OP code
+      .storeUint(increment_by, 32) // increment_by value
+      .endCell();
+
     await provider.internal(sender, {
       value, // сумма TON в формате nano
       sendMode: SendMode.PAY_GAS_SEPARATELY,
-      body: beginCell().endCell(), // пустая йчейка
+      body: msg_body,
     });
   }
 
   // геттер который возвращает адрес из c4 хранилища
   async getData(provider: ContractProvider) {
-    const { stack } = await provider.get("get_the_latest_sender", []);
+    const { stack } = await provider.get("get_contract_storage_data", []);
     return {
+      number: stack.readNumber(),
       recent_sender: stack.readAddress(),
     };
   }
