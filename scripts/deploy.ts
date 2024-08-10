add0a394500dd840fad316f061e08b26145a92bc
evilcore29 <evilcore29@gmail.com>
2024-08-10 15:18:09 +0300

    TON Stepik Course: Lesson 4.6

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -3,12 +3,19 @@ import { MainContract } from "../wrappers/MainContract";
 import { compile, NetworkProvider } from "@ton/blueprint";
 
 // * Все ваши скрипты должны экспортировать функцию run, если вы хотите, чтобы они работали с Blueprint
+
+// * deployed with tonkeeper https://testnet.tonscan.org/address/EQDAbnsqALKAoQO5uS1qOI8X7OhkeDnv3hZiqg2VAqhPa6xN#transactions
+// * EQDAbnsqALKAoQO5uS1qOI8X7OhkeDnv3hZiqg2VAqhPa6xN
+
+// * deployed with tonhub https://testnet.tonscan.org/address/EQAh3XVJheA_fL83ZfhxEfN3rUZ9-FTQS4F8WvEQPZATXu1d#transactions
+// * EQAh3XVJheA_fL83ZfhxEfN3rUZ9-FTQS4F8WvEQPZATXu1d
+
 export async function run(provider: NetworkProvider) {
   const myContract = MainContract.createFromConfig(
     {
       number: 0,
-      address: address("kQDU69xgU6Mj-iNDHYsWWuNx7yRPQC_bNZNCpq5yVc7LiE7D"),
-      owner_address: address("kQDU69xgU6Mj-iNDHYsWWuNx7yRPQC_bNZNCpq5yVc7LiE7D"),
+      address: address("0QA742GqD5wCFMdrxUSATw-z2_65eIdSC_uxtpgMDZxIiU9Y"),
+      owner_address: address("0QA742GqD5wCFMdrxUSATw-z2_65eIdSC_uxtpgMDZxIiU9Y"),
     },
     await compile("MainContract")
   );
